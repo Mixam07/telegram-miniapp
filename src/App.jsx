06a72a3e0575a310b5dcf3718351de3c56c498ca
Maksym <max.mixam07.max@gmail.com>
2025-05-15 18:54:37 +0300

    додав зміни

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [userId, setUserId] = useState("");
+  const [log, setLog] = useState("Waiting...");
+  const [raw, setRaw] = useState("Loading...");
 
   useEffect(() => {
     const tg = window.Telegram.WebApp;
@@ -19,6 +21,31 @@ const App = () => {
     alert("Скопійовано: " + userId);
   };
 
+  try {
+    window.Telegram.WebApp.ready();
+    setRaw(window.Telegram.WebApp.initData || 'No initData')
+
+    fetch('http://localhost:3000/v1/auth/telegram', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: { gameId: 'fabda0e7-356f-4cfd-91ec-b992d9beaa7e', initData: window.Telegram.WebApp.initData }
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('Server response:', data);
+      setLog(JSON.stringify(data, null, 2));
+    })
+    .catch(err => {
+      console.error('Fetch error:', err);
+      setLog('Fetch error: ' + err.message);
+    });
+  } catch (err) {
+    console.error('Script error:', err);
+    setLog('Script error: ' + err.message);
+  }
+
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
       <h2>Telegram Mini App</h2>
@@ -27,6 +54,27 @@ const App = () => {
       <a href="https://example.com" target="_blank" rel="noopener noreferrer">
         🌐 Перейти на сайт
       </a>
+      <h1>Telegram SDK Auth</h1>
+      <h3>initDataRaw:</h3>
+      <pre id="raw">{raw}</pre>
+      <h3>Server response:</h3>
+      <pre id="log">{log}</pre>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/alarmukraine">силка</a>
+      <a href="https://t.me/BotFather">силка на бот</a>
     </div>
   );
 };
